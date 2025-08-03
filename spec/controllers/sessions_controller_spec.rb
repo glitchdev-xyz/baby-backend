e7b59e6c1064e07ef27f7795b07508a0a1cecb5d
Andrea Parthemore <andrea.parthemore@gmail.com>
2025-08-03 14:21:58 -0400

    specs: Add Sessions controller spec.

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+describe SessionsController do
+  # Not concerned about record pollution. Just reloading routes.
+  # rubocop:disable RSpec/BeforeAfterAll
+  after :all do
+    Rails.application.reload_routes!
+  end
+  # rubocop:enable RSpec/BeforeAfterAll
+
+  describe '#create' do
+    before do
+      Rails.application.routes.draw do
+        get '/create', to: "sessions#create"
+      end
+    end
+
+    it 'responds with status unauthorized when user does not authenticate' do
+      allow(User).to receive(:authenticate_by).and_return(nil)
+
+      get :create
+      expect(response.status).to eq 401
+    end
+
+    context 'when a user can authenticate' do
+      it 'responds with status success when user can authenticate' do
+        user = create(:user_with_sessions)
+        allow(User).to receive(:authenticate_by).and_return(user)
+
+        get :create
+        expect(response.status).to eq 200
+      end
+
+      it 'creates a new session for the user' do
+        user = create(:user)
+        allow(User).to receive(:authenticate_by).and_return(user)
+
+        # Set up to spy on user.sessions.create! in
+        # Authentication concern's start_new_session_for
+        allow(user.sessions).to receive(:create!).and_call_original
+
+        get :create
+        expect(user.sessions).to(
+          have_received(:create!)
+            .with(
+              user_agent: request.user_agent,
+              ip_address: request.remote_ip
+            )
+        )
+      end
+
+      # TODO: Figure out how to test this & also,
+      # decide if it's worth testing here. Maybe it could be
+      # asserted indirectly via a integration spec.
+      xit 'sets current_session in global state' do
+        user = create(:user)
+        allow(User).to receive(:authenticate_by).and_return(user)
+
+        #Need to spy on the global Current obj within controller context
+        allow().to receive(:set)
+        get :create
+
+        expect().to  have_received(:session=)
+      end
+    end
+  end
+end
