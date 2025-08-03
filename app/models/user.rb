5780c3bea49ebd1ecd4c833f6ac0cac639f7a658
Andrea Parthemore <andrea.parthemore@gmail.com>
2025-08-03 18:40:27 -0400

    user: Begin adding stats.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,21 @@ class User < ApplicationRecord
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
   validates :email_address, presence: true, uniqueness: true
+
+  def total_games_played
+    stats[:games].reduce do |game|
+      game.play_count
+    end
+  end
+
+  def increment_game_play_count(game_name)
+    return nil unless stats[:games][game_name][:play_count]
+  end
+
+  def game_play_count(game_name)
+    # This line needs a lot of help, but don't want to get hung up on it right now.
+    return nil unless stats && stats['games'] && stats['games'][game_name] && stats['games'][game_name]['play_count']
+
+    stats['games'][game_name]['play_count']
+  end
 end
