4c1b2b0b7b2bcf873f288bf449bdf591c4542f7d
Andrea Parthemore <andrea.parthemore@gmail.com>
2025-08-04 08:14:48 -0400

    stats-endpoint: Add subscription status.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  require 'net/http'
   allow_unauthenticated_access only: %i[ create ]
 
   def create
@@ -22,7 +23,8 @@ class UsersController < ApplicationController
   def stats
     user = User.find(Current.user.id)
     count = GameEvent.where(user_id: user.id).count
-    payload =    {
+    subscription_status = subscription_status(user)
+    payload = {
       user: {
         id: user.id,
         email: user.email_address,
@@ -31,6 +33,32 @@ class UsersController < ApplicationController
         }
       }
     }
-    render json: payload
+    if subscription_status
+      payload[:user].merge!(subscription_status)
+    else
+      payload[:user].merge!({ subscription_status: 'error' })
+    end
+
+    render json: payload.to_json
+  end
+
+  private def subscription_status(user)
+    uri = URI.parse(billing_url(user))
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request['Authorization'] = "Bearer #{billing_token}"
+    response = http.request(request)
+
+    return nil unless response.is_a?(Net::HTTPSuccess)
+
+    JSON.parse(response.body)
+  end
+
+  private def billing_url(user)
+    "https://interviews-accounts.elevateapp.com/api/v1/users/#{user.id}/billing"
+  end
+  # TODO: Prototype only
+  private def billing_token
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJiaWxsaW5nIiwiaWF0IjoxNzQzMDg1NDk5LCJleHAiOm51bGwsImF1ZCI6Ind3dy5leGFtcGxlLmNvbSIsInN1YiI6ImJpbGxpbmdfY2xpZW50In0.aRwnR_QP6AlOv_JanMkbhwe9ACDcJc5184pXdR0ksXg'
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'webmock/rspec'
 
 describe UsersController do
   describe 'POST create' do
@@ -34,27 +35,77 @@ describe UsersController do
   end
 
   describe 'GET stats' do
-    it 'returns the user\'s stats' do
-      user = create(:user_with_sessions, sessions_count: 1)
-
-      ['Zelda II', 'Kingdom of Kroz'].each do |game_name|
-        GameEvent.create(user_id: user.id, event_type: 'COMPLETED', game_name:)
+    let(:user) { create(:user_with_sessions, sessions_count: 1) }
+    context 'with a successful billing api response' do
+      let(:billing_response) { { subscription_status: 'active' } }
+      before do
+        stub_request(:get, "http://interviews-accounts.elevateapp.com:443/api/v1/users/#{user.id}/billing").
+          with(
+            headers: {
+	      'Accept'=>'*/*',
+	      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+	      'Authorization'=>'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJiaWxsaW5nIiwiaWF0IjoxNzQzMDg1NDk5LCJleHAiOm51bGwsImF1ZCI6Ind3dy5leGFtcGxlLmNvbSIsInN1YiI6ImJpbGxpbmdfY2xpZW50In0.aRwnR_QP6AlOv_JanMkbhwe9ACDcJc5184pXdR0ksXg',
+	      'User-Agent'=>'Ruby'
+            }).
+          to_return(status: 200, body: billing_response.to_json, headers: {})
       end
 
-      request.headers['Authorization'] = "Token token=\"#{user.sessions.first.token}\""
-      get(:stats)
+      it 'returns the user\'s stats' do
 
-      expected_response = {
-        user: {
-          id: user.id,
-          email: user.email_address,
-          stats: {
-            total_games_played: 2
+
+        ['Zelda II', 'Kingdom of Kroz'].each do |game_name|
+          GameEvent.create(user_id: user.id, event_type: 'COMPLETED', game_name:)
+        end
+
+        request.headers['Authorization'] = "Token token=\"#{user.sessions.first.token}\""
+        get(:stats)
+
+        expected_response = {
+          user: {
+            id: user.id,
+            email: user.email_address,
+            stats: {
+              total_games_played: 2,
+            }
+          }.merge(billing_response)
+        }.to_json
+
+        expect(response.body).to eq expected_response
+      end
+    end
+
+    context 'with an error response from the billing api' do
+      before do
+        stub_request(:get, "http://interviews-accounts.elevateapp.com:443/api/v1/users/#{user.id}/billing").
+          with(
+            headers: {
+	      'Accept'=>'*/*',
+	      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+	      'User-Agent'=>'Ruby'
+            }).
+          to_return(status: 401)#, body: billing_response.to_json, headers: {})
+      end
+
+      it 'returns the user\'s stats with an error indicated in billing key' do
+        ['Zelda II', 'Kingdom of Kroz'].each do |game_name|
+          GameEvent.create(user_id: user.id, event_type: 'COMPLETED', game_name:)
+        end
+
+        request.headers['Authorization'] = "Token token=\"#{user.sessions.first.token}\""
+        get(:stats)
+        expected_response = {
+          user: {
+            id: user.id,
+            email: user.email_address,
+            stats: {
+              total_games_played: 2
+            },
+            subscription_status: 'error'
           }
-        }
-      }.to_json
+        }.to_json
 
-      expect(response.body).to eq expected_response
+        expect(response.body).to eq expected_response
+      end
     end
   end
 end
